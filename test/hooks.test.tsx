b353495a013270d1d77c49cfd99e5a7c1d44060e
Hendrik Mans <hendrik@mans.de>
2020-12-16 18:15:48 +0000

    Better test

--- test/hooks.test.tsx
+++ test/hooks.test.tsx
@@ -16,13 +16,32 @@ describe("useStore", () => {
   })
 
   it("provides a proxy that allows subscribing to individual properties", async () => {
-    const store = makeStore({ wood: 8, houses: 0 })
+    const store = makeStore({
+      wood: 8,
+      gold: 4,
+      houses: 0
+    })
 
-    const collectWood = () => store.set((state) => ({ wood: state.wood + 1 }))
+    const collectWood = () =>
+      store.set((state) => ({
+        wood: state.wood + 1
+      }))
+
+    const sellWood = () =>
+      store.set((state) => ({
+        wood: state.wood - 1,
+        gold: state.gold + 1
+      }))
+
     const buildHouse = () =>
-      store.set((state) => ({ wood: state.wood - 10, houses: state.houses + 1 }))
+      store.set((state) => ({
+        wood: state.wood - 5,
+        gold: state.gold - 5,
+        houses: state.houses + 1
+      }))
 
     let woodRenderCount = 0
+    let goldRenderCount = 0
     let housesRenderCount = 0
     let buttonsRenderCount = 0
 
@@ -30,6 +49,12 @@ describe("useStore", () => {
       woodRenderCount++
       const { wood } = useStore(store)
       return <p>Wood: {wood}</p>
+    }
+
+    const Gold = () => {
+      goldRenderCount++
+      const { gold } = useStore(store)
+      return <p>Gold: {gold}</p>
     }
 
     const Houses = () => {
@@ -43,6 +68,7 @@ describe("useStore", () => {
       return (
         <p>
           <button onClick={collectWood}>Collect Wood</button>
+          <button onClick={sellWood}>Sell Wood</button>
           <button onClick={buildHouse}>Build House</button>
         </p>
       )
@@ -51,6 +77,7 @@ describe("useStore", () => {
     const { getByText, findByText } = render(
       <>
         <Wood />
+        <Gold />
         <Houses />
         <Buttons />
       </>
@@ -58,19 +85,27 @@ describe("useStore", () => {
 
     await findByText("Wood: 8")
     await findByText("Houses: 0")
+    await findByText("Gold: 4")
 
     fireEvent.click(getByText("Collect Wood"))
     fireEvent.click(getByText("Collect Wood"))
+    fireEvent.click(getByText("Collect Wood"))
+
+    await findByText("Wood: 11")
+
+    fireEvent.click(getByText("Sell Wood"))
 
     await findByText("Wood: 10")
+    await findByText("Gold: 5")
     await findByText("Houses: 0")
 
     fireEvent.click(getByText("Build House"))
 
-    await findByText("Wood: 0")
+    await findByText("Wood: 5")
+    await findByText("Gold: 0")
     await findByText("Houses: 1")
 
-    expect(woodRenderCount).toEqual(4)
+    expect(woodRenderCount).toEqual(6)
     expect(housesRenderCount).toEqual(2)
     expect(buttonsRenderCount).toEqual(1)
   })
